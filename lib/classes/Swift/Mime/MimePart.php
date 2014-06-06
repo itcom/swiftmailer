d6f527a07c654d55a6ce6da7368d53c3c7f79c9c
Atsuji Okuyama <okuyama@workgroup.jp>
2014-06-06 23:33:05 +0900

    japanese iso-2022-jp

--- lib/classes/Swift/Mime/MimePart.php
+++ lib/classes/Swift/Mime/MimePart.php
@@ -200,7 +200,10 @@ class Swift_Mime_MimePart extends Swift_Mime_SimpleMimeEntity
     protected function _convertString($string)
     {
         $charset = strtolower($this->getCharset());
-        if (!in_array($charset, array('utf-8', 'iso-8859-1', ''))) {
+		if (in_array($charset, array('iso-2022-jp'))) {
+			$string = mb_convert_encoding($string, $charset, 'utf-8');
+		}
+        if (!in_array($charset, array('utf-8', 'iso-8859-1', 'iso-2022-jp', ''))) {
             // mb_convert_encoding must be the first one to check, since iconv cannot convert some words.
             if (function_exists('mb_convert_encoding')) {
                 $string = mb_convert_encoding($string, 'utf-8', $charset);
